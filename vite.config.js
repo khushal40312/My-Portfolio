d032d5f3b39458f8c04f35b901832c755cf26d60
khushal <khushalsharma40312@gmail.com>
2025-03-09 09:45:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3000, // Adjust the limit (default is 500KB)
+    chunkSizeWarningLimit: 5000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor dependencies
+          }
+        },
+      },
+    },
   },
 })
