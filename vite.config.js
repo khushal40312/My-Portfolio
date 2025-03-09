b8ceff02a1699bc3a37b39cd8d29e686d5d1fda5
khushal <khushalsharma40312@gmail.com>
2025-03-09 10:02:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 5000, 
+    chunkSizeWarningLimit: 5000, // Avoid warnings for larger models
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Separate vendor dependencies
-          }
+          if (id.includes('models')) return 'models'; // Separate models into a chunk
         },
       },
     },
   },
 })
+
